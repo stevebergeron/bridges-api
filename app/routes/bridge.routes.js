494e67176572a5a64317e4c9d424cd8e3ad6c77f
Steve Bergeron <steve.bergeron1@gmail.com>
2020-07-23 23:53:04 -0400

    ...

--- app/routes/bridge.routes.js
+++ app/routes/bridge.routes.js
@@ -16,4 +16,10 @@ module.exports = app => {
   router.get("/wgn/:wgn", bridges.findByWGN);
 
   app.use('/api/bridges', router);
+  
+  app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 };
