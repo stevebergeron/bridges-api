eba2a6c926571c62d9412a00fe52f60ec10a68b1
Steve Bergeron <steve.bergeron1@gmail.com>
2020-07-24 00:07:34 -0400

    ...

--- app/routes/bridge.routes.js
+++ app/routes/bridge.routes.js
@@ -3,7 +3,7 @@ module.exports = app => {
 
   var router = require("express").Router();
 
-  router.all('/', function(req, res, next) {
+  router.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
