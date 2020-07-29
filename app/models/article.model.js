71ea512bd3138e83f28cad000f77036d5d91fe62
Steve Bergeron <steve.bergeron1@gmail.com>
2020-07-29 14:02:44 -0400

    Had to alter wgn column and added columns for photo captions and date_taken

--- app/models/article.model.js
+++ app/models/article.model.js
@@ -6,7 +6,7 @@
  */
 module.exports = (sequelize, Sequelize) => {
   const Article = sequelize.define("articles", {
-    wgn: Sequelize.STRING(15),
+    wgn: Sequelize.STRING(30),
     seq: Sequelize.INTEGER,
     header: Sequelize.STRING(200),
     text: Sequelize.TEXT
--- app/models/photo.model.js
+++ app/models/photo.model.js
@@ -8,9 +8,11 @@
  */
 module.exports = (sequelize, Sequelize) => {
   const Photo = sequelize.define("photos", {
-    wgn: Sequelize.STRING(15),
+    wgn: Sequelize.STRING(30),
     seq: Sequelize.INTEGER,
-    link: Sequelize.STRING(200)
+    link: Sequelize.STRING(200),
+    date_taken: Sequelize.STRING(25),
+    caption: Sequelize.STRING(300)
   });
 
   return Photo;
